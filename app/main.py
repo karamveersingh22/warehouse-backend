29d53cbb4979c740c89284256a9bf2124657f57d
karamveersingh22 <karamveersinghsuri@gmail.com>
2026-05-20 03:16:45 +0530

    lock CORS to Vercel URL

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Replace with your Vercel URL in production
+    allow_origins=["https://godown-frontend-rust.vercel.app/"],  # Replace with your Vercel URL in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
